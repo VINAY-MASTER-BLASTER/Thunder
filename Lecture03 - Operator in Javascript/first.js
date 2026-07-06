965feb00c87af0868da6c271b88e04493da349a9
VINAY-MASTER-BLASTER <vinaytoppo145@gmail.com>
2026-07-06 14:15:02 +0530

    Lecture03 - Operator in Javascript

---
+++ Lecture03 - Operator in Javascript/first.js
@@ -0,0 +1,225 @@
+// Primative Data type 
+// let firstnumber = 10
+// let secondnumber = firstnumber
+// let thirdnumber = secondnumber
+
+// firstnumber = 20
+// console.log(firstnumber,secondnumber,thirdnumber)
+
+
+// let obj1  = {
+//     name : "Vinay",
+//     age : 21,
+//     occupation : "Devloper"
+// }
+// let obj2 = obj1
+// obj2.name = "Vikas"
+// console.log(obj1,obj2)
+
+
+
+// Compare by the value
+// let a = 10 
+// let b = 10
+// console.log(a==10)
+
+
+// let a = "Vikas"
+// let b = "Nitesh"
+// console.log(a==b)
+
+// Non Primative Date compare on the basis of reference 
+// let obj = {
+//     name : "Vinay",
+//     age : 21
+// }
+// let obj1 = obj
+// console.log(obj == obj1) Address 
+
+
+// let obj = {
+//     name : "Vinay",
+//     age : 21
+// }
+// let obj1 = {
+//     name : "Vinay",
+//     age : 21
+// }
+
+// console.log(obj == obj1)
+
+
+
+// const a = 10 
+// const b = a 
+// b = 30
+// console.log(b) //Error
+
+
+// const obj = {
+//     name : "Vinay",
+//     age : 21
+// }
+// obj.name = "Vikas"
+// console.log(obj)
+
+
+// const obj = {
+//     name:"vinay",
+//     age : 21
+// }
+
+// obj = {
+//     name : 'ravi',
+//     age : 25
+// }
+// console.log(obj)
+
+
+// let const - mordern methods 
+// var - older methods 
+
+// console.log(a)
+// var a = 10
+
+
+// if(true){
+//     var a = 10
+// }
+// console.log(a)
+
+
+// Operator in JavaScript 
+// Arthmatic Operator 
+
+// console.log(5-10)
+// console.log(50+40)
+// console.log(40/12)
+// console.log(2*5)
+// console.log(10%2)
+// console.log(10**2)
+
+
+// assinment operator 
+// let a = 10 
+// let b = 20
+// a = a+b
+// console.log(a)
+
+
+// let a = 10 
+// let b = 20
+// a+=b
+// a/=b
+// a-=b
+// a*=b
+// console.log(a)
+
+
+// let a = 10
+// post increment a++ // pehle app ke original value le lijiye uske baad a increase hoga
+// console.log(a++)
+// console.log(a)
+
+// pre increment  ++a // pehle mein apni value ko 1 increase karunga uska baad apko value dunga 
+// console.log(++a)
+// console.log(a)
+
+
+// comparison operator 
+// console.log(10>5)
+// console.log(10>=5)   
+// console.log(10<5)
+// console.log(10<=5)
+
+// Gadhe coder ya hurkat karta hai, isko seekhta hai, aur apna time poora waste karta hai 
+// console.log(10=="10")
+// console.log(10 == 10)
+// console.log("vinay"=={name:"vinay"})
+
+
+// Html ke andar  kitna bacho ne from banaye hai 
+// name : "vinay"
+// age : 21
+// city : "Delhi"
+
+// type converstion 
+// let age = Number("21")
+// console.log(typeof age)
+
+
+
+// calculator : 
+// first = "10", second= "20"
+// console.log(Number("10")+Number("20"))
+
+// console.log(30+'7')
+// console.log(30+Number('7'))
+// console.log(String(30)+'7')
+
+
+// console.log(null == undefined)
+// JS : Null is loosely equal to undefind and not to anyone ==
+// console.log(null == 0)
+// console.log(null == 1)
+// console.log(null == false)
+
+// let ab = 10*2+5*64+78/2  // bad way to writting a code 
+// console.log(ab)
+
+// let ab = (((10*2)+5)*64+(74/2)) //good practices write a code 
+// console.log(ab) //clean code 
+
+
+
+// comparison operator aayenge : >=, <= <, >  || null convert to zero 0
+// console.log(null>=0)
+// console.log(null<=0)
+// console.log(null>0)
+// console.log(null<0)
+
+
+
+// console.log(10 === 10)
+// console.log(10 === '10')
+// === type ko cheak, fr apki value ko compare karta hai 
+
+
+// console.log(0==false)
+
+
+// Operator important hai
+// Logical Operator 
+
+// AND 
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false && true)
+// console.log(false && false)
+
+
+// OR 
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || true)
+// console.log(false || false)
+
+
+
+// console.log(true && "Vinay")
+// console.log(true && true)
+// console.log(true && 10)
+// first condition true then second condition return
+
+// console.log("" && true) 
+// console.log("hello" && true) 
+// console.log(0 && true) 
+// console.log(false && true)
+// first condition is false return it the falsy value
+
+
+
+
+// console.log(false || "Rohit")
+// console.log(true || true)
+// same to OR operator 
