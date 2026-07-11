052930a1083118b509556f4d45b8332d48a3d120
VINAY-MASTER-BLASTER <vinaytoppo145@gmail.com>
2026-07-11 13:44:58 +0530

    Lecture07 - Objects in Javascript

---
+++ Lecture07 - Objects in Javascript/Array.js
@@ -0,0 +1,47 @@
+// Homewrok : sorting function , how does it work 
+
+// const name = ['vinay', "Ajay", 'Rohit', 'Sagar', 'Deepak', 'Vinay']
+
+
+// Ascii table : R or r A: 65 b: 66 , a : 97
+// sort : accending order 
+// name.sort()
+// name.reverse()
+// console.log(name)
+
+
+// const number = [10,30,45,46,90,23,67,2,101]
+// ["10","30","45","46","90","23","67","2"]
+// it's considering each element as a string by defullt 
+// '2' '10' it;s treated as character
+// number.sort()
+// console.log(number)
+
+// if it's treated as a number 
+
+// Ascending
+// number.sort((a,b)=>a-b)
+
+// Descending
+// number.sort((a,b)=>b-a)
+// console.log(number)
+
+
+
+
+
+// const num = [10,30,45,46,90,23,67,2,101]
+// String ke jaise hur ek chej ko treat kar raha hai 
+// num.sort()
+
+// const arr = [10,'vinay', true, 33, 'vikas', null] //in array any type of data it;s conver to string
+//Hypothotical shoution
+
+// arr.sort()
+// hur ek ko string mein convert kar dunga uska baad sort karunga
+//  ['10','vinay', 'true', '33', 'vikas', 'null'] //conseder as a sting each element 
+// console.log(arr)
+
+
+
+
