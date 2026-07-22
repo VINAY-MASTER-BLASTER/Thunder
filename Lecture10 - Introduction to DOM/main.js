748efec0156a08036ae5df62f75052fe50edf3c4
VINAY-MASTER-BLASTER <vinaytoppo145@gmail.com>
2026-07-22 22:52:08 +0530

    Lecture10 - Introduction to DOM

---
+++ Lecture10 - Introduction to DOM/main.js
@@ -0,0 +1,70 @@
+// access the element 
+// document.getElementById('first')
+
+// assign the element in the variable
+// const element = document.getElementById('first') // retune the object 
+
+// change the content 
+// element.textContent = "Hello JavaScript"
+// console.log(element)
+
+
+
+// const list = document.getElementsByTagName('li')
+// console.log(list)
+// console.log(list[0])
+// console.log(list[1])
+// console.log(list[2])
+// console.log(list[3])
+
+
+// const heading = document.getElementsByClassName('Herader')
+// console.log(heading[0])
+// console.log(heading[1])
+
+
+// Array - class multiple element same type ka ho 
+//  id Uniqe hoti hai retune one sigle element 
+
+
+// In Html collection is the of Array but I't not a proper array 
+// how looping in Array
+
+// const a = [...heading]
+// a.forEach(element => {
+//     console.log(element)
+// });
+
+
+// const element = document.getElementById('first')
+// element.style.backgroundColor = 'blue'
+// element.style.fontSize = '110px'
+
+// HtML Collection - Object 
+
+
+// element.textContent //all ways use textcontent (Safe)
+// console.log(element) //actully content and hidden content show  
+
+
+// element.innerHTML  //not use it 
+// console.log(element)  // actully content inside tag and hidden content
+// what ever pass it consider as a HTML
+// element.innerHTML = '<a href="https://www.youtube.com/" target="_blank">Visit Example Website</a>' // Dangerous Sign --> code ka execution ho jata hai 
+
+
+// element.innerText
+// console.log(element) //jo content screen par hai vo hee show karga 
+
+
+// classname 
+// const heading = document.getElementsByClassName('Herader')
+// console.log(heading)
+
+
+// heading[0].id = "12"
+// console.log(heading[0])
+
+
+// heading[1].classList.add("Dev")
+// console.log(heading[1])
